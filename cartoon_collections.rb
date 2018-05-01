dd0ac306039b46ba31fde5d41c870da15d332029
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-01 21:47:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.detect do |cheese_types.detect|
+    array.detect do |cheese_types|
   end
 
 end
